7e7dedb680cc7ded69a03f901ba9cab0d6fd05f2
tacumihaela <72128040+tacumihaela@users.noreply.github.com>
2020-11-28 14:09:27 +0200

    Update app.js

--- main/app.js
+++ main/app.js
@@ -1,5 +1,31 @@
-function addTokens(input, tokens){
-    
+function addTokens(input, tokens) {
+    if (typeof input !== "string") {
+        throw new Error("Invalid input");
+
+    }
+    if (input.length < 6) {
+        throw new Error("Input should have at least 6 characters");
+    }
+
+    if (tokens.filter(product => !(typeof product.tokenName === "string")).length) {
+        throw new Error("Invalid array format");
+
+    }
+    if (input.search("...") !== -1) {
+
+        for (let e of tokens) {
+            let v="${"+e.tokenName+"}";
+            input=input.replace("...",v);
+
+        }
+
+    }
+    return input;
+
+
+
+
+
 }
 
 const app = {
